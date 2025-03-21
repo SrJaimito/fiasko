5739dc865f67a7fa392efdc84149ec611f7e229b
SrJaimito <jaime.senors@upm.es>
2025-03-21 08:01:20 +0100

    Create BlockDiagramModel header

---
+++ src/block_diagram/model/BlockDiagramModel.h
@@ -0,0 +1,16 @@
+#ifndef __BLOCK_DIAGRAM_MODEL_H__
+#define __BLOCK_DIAGRAM_MODEL_H__
+
+#include "BlockModel.h"
+#include "ConnectorModel.h"
+
+class BlockDiagramModel {
+
+    ////////////////////////////////////////// Attributes
+
+    ////////////////////////////////////////// Methods
+
+};
+
+#endif
+
