f4fd7b0fc4586e605cdf169d789d601305e2623e
SrJaimito <jaime.senors@upm.es>
2025-03-22 17:03:56 +0100

    Redefine BlockDiagramModel header

--- src/block_diagram/model/BlockDiagramModel.h
+++ src/block_diagram/model/BlockDiagramModel.h
@@ -1,14 +1,29 @@
 #ifndef __BLOCK_DIAGRAM_MODEL_H__
 #define __BLOCK_DIAGRAM_MODEL_H__
 
+#include <vector>
+
 #include "BlockModel.h"
-#include "ConnectorModel.h"
+#include "WireModel.h"
 
 class BlockDiagramModel {
 
     ////////////////////////////////////////// Attributes
 
+    private:
+        std::vector<BlockModel *> blocks;
+        std::vector<WireModel *> wires;
+
     ////////////////////////////////////////// Methods
+
+    public:
+        ~BlockDiagramModel();
+
+        void addBlock(BlockModel *block);
+        void removeBlock(BlockModel *block);
+
+        void addWire(WireModel *wire);
+        void removeWire(WireModel *wire);
 
 };
 
